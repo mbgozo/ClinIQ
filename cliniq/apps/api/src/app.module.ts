7f6c502b12ba50f8df9828cc06e672612a5c1c1d
Micky <mbgozobuilds@gmail.com>
2026-03-31 10:31:30 +0000

    Add QaModule to app.module.ts imports for enhanced functionality

--- cliniq/apps/api/src/app.module.ts
+++ cliniq/apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
+import { QaModule } from "./qa/qa.module";
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, QaModule],
   controllers: [],
   providers: []
 })
